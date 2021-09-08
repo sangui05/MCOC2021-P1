1b3ff7299a2faffceb22240a0efacdda9f43f03b
meetcheverry <69227038+meetcheverry@users.noreply.github.com>
2021-09-08 14:02:16 -0300

    Update barra.py

--- barra.py
+++ barra.py
@@ -1,25 +1,23 @@
 import numpy as np
+from constantes import g_, ρ_acero, E_acero
 
-g = 9.81 #kg*m/s^2
 
 class Barra(object):
 
 	"""Constructor para una barra"""
-	def __init__(self, ni, nj, R, t, E, ρ, σy):
+	def __init__(self, ni, nj, seccion):
 		super(Barra, self).__init__()
 		self.ni = ni
 		self.nj = nj
-		self.R = R
-		self.t = t
-		self.E = E
-		self.ρ = ρ
-		self.σy = σy
+		self.seccion = seccion
+		
+	
 
 	def obtener_conectividad(self):
 		return [self.ni, self.nj]
 
 	def calcular_area(self):
-		A = np.pi*(self.R**2) - np.pi*((self.R-self.t)**2)
+		A = seccion.area
 		return A
 
 	def calcular_largo(self, reticulado):
@@ -33,13 +31,10 @@ class Barra(object):
 		return np.sqrt(np.dot(dij,dij))
 
 	def calcular_peso(self, reticulado):
-		"""Devuelve el largo de la barra.
-		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
-		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
-		"""
+		
 		L = self.calcular_largo(reticulado)
-		A = self.calcular_area()
-		return self.ρ * A * L * g
+		Peso = seccion.peso
+		return Peso * L
 '''
 
     def obtener_rigidez(self, ret):
