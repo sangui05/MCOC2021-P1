9d036dac56f536dee91240afed9a693026d969b6
carmenbenavente <cbenavente@miuandes.cl>
2021-09-20 23:23:31 -0300

    Avance secciones ICHA

--- secciones.py
+++ secciones.py
@@ -40,19 +40,56 @@ class SeccionICHA(object):
         super(SeccionICHA, self).__init__()
         self.denominacion = denominacion
         self.color = color  #color para la seccion
+        palabras = []
+        palabra = ''
+        for i in self.denominacion:
+            if i == 'H':
+                j = self.denominacion.index(i) +1
+                if self.denominacion[j] == 'R':
+                    self.base_datos = pd.read_excel(base_datos,sheet_name='HR', header=None)
+                else:
+                    self.base_datos = pd.read_excel(base_datos, sheet_name='H', header=None)
+                continue
 
-        
+            if i == 'x':
+                palabras.append(float(palabra))
+                palabra = ''
+
+            else:
+                palabra += i
+
+        palabras.append(float(palabra))
+
+
+
+        for i in range(0, len(self.base_datos[1])):
+            if self.base_datos[1][i] == palabras[0]:
+                if self.base_datos[3][i] == palabras[1]:
+                    if self.base_datos[5][i] == palabras[2]:
+                        self.posicion = i
+
+
     def area(self):
-        return 0
+
+        area = self.base_datos[9][self.posicion]
+
+        return area
 
     def peso(self):
+
+        peso = self.base_datos[3][self.posicion]
+
         return 0
 
     def inercia_xx(self):
-        return 0
+
+        inercia_xx = self.base_datos[10][self.posicion]
+        return inercia_xx
 
     def inercia_yy(self):
-        return 0
+
+        inercia_yy = self.base_datos[14][self.posicion]
+        return inercia_yy
 
     def __str__(self):
-        return f"Seccion ICHA {self.denominacion}"
+        return f"Seccion ICHA {self.denominacion}, area {self.area()}"
