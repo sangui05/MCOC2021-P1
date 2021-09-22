a227d90f37449d17d459c295ec5c6ac4b199db3e
sangui05 <88542346+sangui05@users.noreply.github.com>
2021-09-22 20:50:03 -0300

    entrega 3

--- reticulado.py
+++ reticulado.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from scipy.linalg import solve
 from barra import Barra
 import numpy as np
@@ -53,6 +54,238 @@ class Reticulado(object):
         pesototalbarras = 0
         for barra in self.barras:
             w_barra= barra.calcular_peso(self)
+            pesototalbarras+= w_barra
+        
+        return pesototalbarras
+
+    def obtener_nodos(self):
+        
+        return self.xyz
+
+    def obtener_barras(self):
+        
+        return self.barras
+
+
+
+    def agregar_restriccion(self, nodo, gdl, valor=0.0):
+        print(f"Quiero agregar una restriccion en: ({nodo} {gdl} {valor})")
+        
+        if self.restricciones[nodo].index(nodo) == ValueError: #Si el nodo no está en las restricciones, se agrega
+            
+            self.restricciones[nodo] = []
+            self.restricciones[nodo].append(gdl, valor)
+            return 0
+        else :
+            self.restricciones[nodo].append(gdl, valor)
+            return 0
+        
+       # print (restricciones)
+        
+
+        return 0
+
+    def agregar_fuerza(self, nodo, gdl, valor):
+        
+        print(f"Quiero agregar una fuerza en: ({nodo} {gdl} {valor})")
+
+        if self.cargas.index(nodo) == ValueError : #Si no existe el nodo, se agrega.
+            
+            self.cargas[nodo] = []
+            self.cargas[nodo].append(gdl, valor)
+            return 0
+        else:
+                    
+            self.cargas[nodo].append(gdl, valor)
+            return 0
+        
+            #print (cargas)
+        
+        
+       
+
+
+    def ensamblar_sistema(self):
+        
+        for e in self.barras: #recore las barras #barras tiene [N°barra | ni | nj]
+            ni = self.barras[1] 
+            nj = self.barras[2]
+            
+            ke = e.obtener_rigidez()
+            fe = e.obtener_vector_de_carga()
+            
+            d = [3*ni,3*ni+1, 3*ni+2,3*nj,3*nj+1,3*nj+2]
+            
+            for i in range(6):
+                p = d[i]
+                for j in range(6):
+                    q = d[j]
+                    K[p,q] += k_e[i,j]
+                f[p] += f_e[i]
+            
+            #agregar cargas puntuales
+            
+            for nodo in cargas:
+                    print(nodo)
+                    Ncargas = len(cargas[nodo])
+                    print(Ncargas)
+                    
+                    for carga in range :
+                        
+                        gdl = cargas[nodo][0]
+                        f = cargas[nodo][1]
+                        print(f"Agregando carga de {f} en GDL {gdl}")
+                        
+                        gdl_global = 3*node + gdl
+                        F[gdl_global] += f
+           # 
+           #
+           #
+           
+           #self.K #matriz rigidez
+           #self.u # grados de libertad
+        return 0
+
+
+
+    def resolver_sistema(self):
+        
+        """Implementar"""	
+        # A DEFININIR
+        
+        #self.Ff
+        #self.Fc
+        #self.Kcc
+        #self.Kff
+        #self.Kfc
+        #self.Kcf
+        
+        #self.u
+        #self.uf
+        #self.uc
+        
+        #self.R REACCIONES
+        
+        #para graficar ret.u
+        
+        return 0
+
+    def obtener_desplazamiento_nodal(self, n):
+        
+        """Implementar"""	
+        
+        return 0
+
+
+    def obtener_fuerzas(self):
+        
+        """Implementar"""	
+        
+        return 0
+
+
+    def obtener_factores_de_utilizacion(self, f):
+        
+        """Implementar"""	
+        
+        return 0
+
+    def rediseñar(self, Fu, ϕ=0.9):
+        
+        """Implementar"""	
+        
+        return 0
+
+
+
+    def chequear_diseño(self, Fu, ϕ=0.9):
+        
+        """Implementar"""	
+        
+        return 0
+
+
+
+
+
+
+
+    def __str__(self):
+
+        s = "Soy un reticulado :) \n"
+
+        s += "Nodos: \n"
+
+        for i in range(self.Nnodos):
+            s += f"{i} : ({self.xyz[i][0]}, {self.xyz[i][1]}, {self.xyz[i][2]})\n"
+            i += 1
+
+        s += "Barras: \n"
+
+        h = 0
+        for i in self.barras:
+            s += f"{h} : [{self.barras[h].ni} {self.barras[h].nj}]\n"
+            h += 1
+
+        return s
+
+=======
+import numpy as np
+from scipy.linalg import solve
+from barra import Barra
+import numpy as np
+from scipy.linalg import solve
+from barra import Barra
+#bar = Barra()
+class Reticulado(object):
+    """Define un reticulado"""
+    __NNodosInit__ = 100
+
+    #constructor
+    def __init__(self):
+        super(Reticulado, self).__init__()
+        
+        print("Constructor de Reticulado")
+        
+        self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
+        self.Nnodos = 0
+        self.barras = []
+        self.cargas = {}
+        self.restricciones = {}
+        
+        
+
+
+    def agregar_nodo(self, x, y, z=0):
+        
+        
+
+        print(f"Quiero agregar un nodo en ({x} {y} {z})")
+        numero_de_nodo_actual = self.Nnodos
+
+        self.xyz[numero_de_nodo_actual,:] = [x, y, z]
+
+        self.Nnodos += 1
+        
+        return 0
+
+    def agregar_barra(self, barra):
+        
+        self.barras.append(barra)        
+        
+        return 0
+
+    def obtener_coordenada_nodal(self, n):
+        
+        corn = self.xyz[n,:]
+        
+        
+        print(f"la posicion del nodo {n} es en las cordenadas =  {corn}")
+        return corn
+    def calcular_peso_total(self):
+        pesototalbarras = 0
+        for barra in self.barras:
+            w_barra= barra.calcular_peso()
             pesototalbarras+= w_barra
         
         return pesototalbarras
@@ -153,142 +386,4 @@ class Reticulado(object):
 
         return s
 
-import numpy as np
-from scipy.linalg import solve
-
-class Reticulado(object):
-    """Define un reticulado"""
-    __NNodosInit__ = 100
-
-    #constructor
-    def __init__(self):
-        super(Reticulado, self).__init__()
-        
-        print("Constructor de Reticulado")
-        
-        self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
-        self.Nnodos = 0
-        self.barras = []
-        self.cargas = {}
-        self.restricciones = {}
-        """Implementar"""	
-        
-
-
-    def agregar_nodo(self, x, y, z=0):
-        
-        """Implementar"""	
-
-        print(f"Quiero agregar un nodo en ({x} {y} {z})")
-        numero_de_nodo_actual = self.Nnodos
-
-        self.xyz[numero_de_nodo_actual,:] = [x, y, z]
-
-        self.Nnodos += 1
-        
-        return 0
-
-    def agregar_barra(self, barra):
-        
-        self.barras.append(barra)        
-        
-        return 0
-
-    def obtener_coordenada_nodal(self, n):
-        
-        """Implementar"""	
-        
-        return 0
-
-    def calcular_peso_total(self):
-        
-        """Implementar"""	
-        
-        return 0
-
-    def obtener_nodos(self):
-        
-        return self.xyz
-
-    def obtener_barras(self):
-        
-        return self.barras
-
-
-
-    def agregar_restriccion(self, nodo, gdl, valor=0.0):
-        
-        """Implementar"""	
-        
-        return 0
-
-    def agregar_fuerza(self, nodo, gdl, valor):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-    def ensamblar_sistema(self, factor_peso_propio=0.):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-
-    def resolver_sistema(self):
-        
-        """Implementar"""	
-        
-        return 0
-
-    def obtener_desplazamiento_nodal(self, n):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-    def obtener_fuerzas(self):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-    def obtener_factores_de_utilizacion(self, f):
-        
-        """Implementar"""	
-        
-        return 0
-
-    def rediseñar(self, Fu, ϕ=0.9):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-
-    def chequear_diseño(self, Fu, ϕ=0.9):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-
-
-
-
-
-    def __str__(self):
-
-        s = "Soy un reticulado :)"
-
-        s += "\n"
-        
-        s += str(self.xyz[0 : self.Nnodos,:])
-
-        return s
+>>>>>>> 6d74533092e64a36f939a146f155ef273688fe5a
